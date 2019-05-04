2a6e9ad36a9ae9dd0507ca74f06c40b03081905d
Doreyd Mehila <dmehila@gmail.com>
2019-05-04 02:17:38 +0200

    Set up the whole module in a function that would take in the elment to drag as input and turns it into a draggable element

--- drag.js
+++ drag.js
@@ -1,45 +1,53 @@
-// *********************************************************
-// **************** get & set position *********************
-// *********************************************************
-const getPosition = element => [element.offsetTop, element.offsetLeft];
-function changePosition(element, elementTop, elementLeft) {
-  if (elementTop !== "same") element.style.top = elementTop + "px";
-  if (elementLeft !== "same") element.style.left = elementLeft + "px";
-}
-// *********************************************************
-// **************** drag & drop ****************************
-// *********************************************************
-function allowDrop(event) {
-  event.preventDefault();
-}
-function drop(event) {
-  event.preventDefault();
-}
+const dragging = (elem, container = document.body) => {
+  // *********************************************************
+  // **************** get & set position *********************
+  // *********************************************************
+  const getPosition = element => [element.offsetTop, element.offsetLeft];
+  function changePosition(element, elementTop, elementLeft) {
+    if (elementTop !== "same") element.style.top = elementTop + "px";
+    if (elementLeft !== "same") element.style.left = elementLeft + "px";
+  }
+  // *********************************************************
+  // **************** drag & drop ****************************
+  // *********************************************************
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  function drop(event) {
+    event.preventDefault();
+  }
 
-function dragStart(element, event) {
-  CursorY2 = event.pageY;
-  CursorX2 = event.pageX;
-  OldPosition = getPosition(element);
-  DeltaY = CursorY2 - OldPosition[0];
-  DeltaX = CursorX2 - OldPosition[1];
-}
+  function dragStart(element, event) {
+    CursorY2 = event.pageY;
+    CursorX2 = event.pageX;
+    OldPosition = getPosition(element);
+    DeltaY = CursorY2 - OldPosition[0];
+    DeltaX = CursorX2 - OldPosition[1];
+  }
 
-function dragging(element, event) {
-  CursorY = event.pageY;
-  CursorX = event.pageX;
-  NewTop = CursorY - DeltaY;
-  NewLeft = CursorX - DeltaX;
-  changePosition(element, NewTop, NewLeft);
-}
-function startDragging(element) {
-  element.onclick = () => {};
-  element.setAttribute("draggable", "true");
-  element.ondragstart = () => dragStart(element, event);
-  element.ondrag = () => dragging(element, event);
-  element.ondragend = () => {};
-}
-function stopDragging(element) {
-  setNewAttribute(element, "draggable", "false");
-  element.ondragstart = () => {};
-  element.ondrag = () => {};
-}
+  function dragging(element, event) {
+    CursorY = event.pageY;
+    CursorX = event.pageX;
+    NewTop = CursorY - DeltaY;
+    NewLeft = CursorX - DeltaX;
+    changePosition(element, NewTop, NewLeft);
+  }
+
+  function startDragging(element) {
+    element.onclick = () => {};
+    element.setAttribute("draggable", "true");
+    element.ondragstart = () => dragStart(element, event);
+    element.ondrag = () => dragging(element, event);
+    element.ondragend = () => {};
+  }
+
+  function stopDragging(element) {
+    setNewAttribute(element, "draggable", "false");
+    element.ondragstart = () => {};
+    element.ondrag = () => {};
+  }
+
+  container.ondrop = () => drop(event);
+  container.ondragover = () => allowDrop(event);
+  startDragging(elem);
+};
